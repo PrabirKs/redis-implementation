a97007bef36b956c53636bca5d6d48c27380ea0d
PrabirKS <pkxsahoo@gmail.com>
2022-12-28 10:29:40 -0500

    producer sent message

---
+++ Redis-Implementation-master/src/main/java/com/example/redisimplementation/UserController.java
@@ -0,0 +1,64 @@
+package com.example.redisimplementation;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    RedisTemplate<String, User> redisTemplate;
+    @Autowired
+    ObjectMapper objectMapper;
+
+    ////////////////////////////////////////////////////////////////////////////////////
+    @PostMapping("/set_value")
+    public void addValue(@RequestParam("key")String key, @RequestBody()User user){
+        redisTemplate.opsForValue().set(key,user);
+    }
+
+    @GetMapping("/get_value")
+    public User getValue(@RequestParam("key")String key){
+        return (User)redisTemplate.opsForValue().get(key); //by default return ype is Object class
+    }
+    //////////////////////////////////////////////////////////////////////////////////////////////
+
+    @PostMapping("/lpush")
+    public void addListValueLeft(@RequestParam("key")String key, @RequestBody()User user){
+        redisTemplate.opsForList().leftPush(key,user);
+    }
+
+    @PostMapping("/rpush")
+    public void addListValueRight(@RequestParam("key")String key, @RequestBody()User user){
+        redisTemplate.opsForList().rightPush(key,user);
+    }
+
+    @GetMapping("/lpop")
+    public User getListValue(@RequestParam("key")String key){
+        return (User)redisTemplate.opsForList().leftPop(key);
+    }
+
+    //////////////////////////////////////////////////////////
+    //it will take the key and converts the object as a map i.e attribute is key and the value is value
+    @PostMapping("/hmset")
+    public void addHashValue(@RequestParam("key")String key,@RequestBody()User user){
+        Map map = objectMapper.convertValue(user,Map.class); //conversion
+        redisTemplate.opsForHash().putAll(key,map);
+    }
+
+    @GetMapping("/hget")  //hashkey is the atrbute name
+    public String getHashValue(@RequestParam("key") String key, @RequestParam("hashKey")String hashKey){
+        return (String)redisTemplate.opsForHash().get(key,hashKey);
+    }
+
+    @GetMapping("/hgetall")
+    public User getHashValueAll(@RequestParam("key")String key){
+        Map map = redisTemplate.opsForHash().entries(key);
+        User user = objectMapper.convertValue(map,User.class);   //objctMapper is for converting
+        return user;
+    }
+}
